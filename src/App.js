3525b2c4522764a548093aa1f59c3ffff55e5a23
JamesRJGray <jamesrjgray@gmail.com>
2023-05-16 21:43:24 -0500

    have basic functioning app after refining app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom'
 import Nav from './components/Nav';
 import Price from './pages/price';
-import Main from './pages/main';
 import Stocks from './pages/stocks';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <Nav />
       <Routes >
-        <Route path='/' element={<Main />} />
         <Route path='/stocks' element={<Stocks />} />
         <Route path='/price/:symbol' element={<Price />} />
       </Routes>
